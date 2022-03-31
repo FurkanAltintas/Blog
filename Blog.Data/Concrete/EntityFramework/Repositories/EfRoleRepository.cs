dbace2cb2c4d26e9bee6bdb5531d498d080b3bd9
Furkan Altıntaş <furkanaltintas785@gmail.com>
2022-03-31 16:10:26 +0300

    Entity Framework RoleRepository Added

---
+++ Blog.Data/Concrete/EntityFramework/Repositories/EfRoleRepository.cs
@@ -0,0 +1,14 @@
+﻿using Blog.Data.Abstract;
+using Blog.Entities.Concrete;
+using Blog.Shared.Data.Concrete.EntityFramework;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog.Data.Concrete.EntityFramework.Repositories
+{
+    public class EfRoleRepository : EfEntityRepositoryBase<Role>, IRoleRepository
+    {
+        public EfRoleRepository(DbContext context) : base(context)
+        {
+        }
+    }
+}
