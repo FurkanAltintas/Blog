821894584e2b43e755394b7efb0c7064d3c212ca
Furkan Altıntaş <furkanaltintas785@gmail.com>
2022-03-31 19:10:00 +0300

    Context Added

--- Blog.Data/Concrete/EntityFramework/Contexts/BlogContext.cs
+++ Blog.Data/Concrete/EntityFramework/Contexts/BlogContext.cs
@@ -1,4 +1,5 @@
-﻿using Blog.Entities.Concrete;
+﻿using Blog.Data.Concrete.EntityFramework.Mappings;
+using Blog.Entities.Concrete;
 using Microsoft.EntityFrameworkCore;
 
 namespace Blog.Data.Concrete.EntityFramework.Contexts
@@ -8,6 +9,15 @@ namespace Blog.Data.Concrete.EntityFramework.Contexts
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer(@"Server=DESKTOP-C8S1J58;Database=Blog;Trusted_Connection=True;Connect Timeout=30;MultipleActiveResultSets=True;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new ArticleMap());
+            modelBuilder.ApplyConfiguration(new CategoryMap());
+            modelBuilder.ApplyConfiguration(new CommentMap());
+            modelBuilder.ApplyConfiguration(new RoleMap());
+            modelBuilder.ApplyConfiguration(new UserMap());
         }
 
         public DbSet<Article> Articles { get; set; }
