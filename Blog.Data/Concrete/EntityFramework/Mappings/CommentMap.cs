9024687bbcf65d71171351e2830eec63ccf0b9d0
Furkan Altıntaş <furkanaltintas785@gmail.com>
2022-03-31 19:10:53 +0300

    CommentMap Added

---
+++ Blog.Data/Concrete/EntityFramework/Mappings/CommentMap.cs
@@ -0,0 +1,70 @@
+﻿using Blog.Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+
+namespace Blog.Data.Concrete.EntityFramework.Mappings
+{
+    public class CommentMap : IEntityTypeConfiguration<Comment>
+    {
+        public void Configure(EntityTypeBuilder<Comment> builder)
+        {
+            builder.HasKey(c => c.Id);
+            builder.Property(c => c.Id).ValueGeneratedOnAdd();
+            builder.Property(c => c.Text).IsRequired();
+            builder.Property(c => c.Text).HasMaxLength(1000);
+            builder.Property(c => c.CreatedByName).IsRequired();
+            builder.Property(c => c.CreatedByName).HasMaxLength(50);
+            builder.Property(c => c.ModifiedByName).IsRequired();
+            builder.Property(c => c.ModifiedByName).HasMaxLength(50);
+            builder.Property(c => c.CreatedDate).IsRequired();
+            builder.Property(c => c.ModifiedDate).IsRequired();
+            builder.Property(c => c.IsActive).IsRequired();
+            builder.Property(c => c.IsDeleted).IsRequired();
+            builder.Property(c => c.Note).HasMaxLength(500);
+            builder.HasOne<Article>(c => c.Article).WithMany(a => a.Comments).HasForeignKey(c => c.ArticleId);
+            builder.ToTable("Comments");
+
+            builder.HasData(
+                new Comment
+                {
+                    Id = 1,
+                    ArticleId = 1,
+                    Text = "1500'lerden beri kullanılmakta olan standard Lorem Ipsum metinleri ilgilenenler için yeniden üretilmiştir. Çiçero tarafından yazılan 1.10.32 ve 1.10.33 bölümleri de 1914 H. Rackham çevirisinden alınan İngilizce sürümleri eşliğinde özgün biçiminden yeniden üretilmiştir.",
+                    IsActive = true,
+                    IsDeleted = false,
+                    CreatedByName = "InitialCreate",
+                    ModifiedByName = "InitialCreate",
+                    CreatedDate = DateTime.Now,
+                    ModifiedDate = DateTime.Now,
+                    Note = "C# Makale Yorumu"
+                },
+                new Comment
+                {
+                    Id = 2,
+                    ArticleId = 2,
+                    Text = "1500'lerden beri kullanılmakta olan standard Lorem Ipsum metinleri ilgilenenler için yeniden üretilmiştir. Çiçero tarafından yazılan 1.10.32 ve 1.10.33 bölümleri de 1914 H. Rackham çevirisinden alınan İngilizce sürümleri eşliğinde özgün biçiminden yeniden üretilmiştir.",
+                    IsActive = true,
+                    IsDeleted = false,
+                    CreatedByName = "InitialCreate",
+                    ModifiedByName = "InitialCreate",
+                    CreatedDate = DateTime.Now,
+                    ModifiedDate = DateTime.Now,
+                    Note = "C++ Makale Yorumu"
+                },
+                new Comment
+                {
+                    Id = 3,
+                    ArticleId = 3,
+                    Text = "1500'lerden beri kullanılmakta olan standard Lorem Ipsum metinleri ilgilenenler için yeniden üretilmiştir. Çiçero tarafından yazılan 1.10.32 ve 1.10.33 bölümleri de 1914 H. Rackham çevirisinden alınan İngilizce sürümleri eşliğinde özgün biçiminden yeniden üretilmiştir.",
+                    IsActive = true,
+                    IsDeleted = false,
+                    CreatedByName = "InitialCreate",
+                    ModifiedByName = "InitialCreate",
+                    CreatedDate = DateTime.Now,
+                    ModifiedDate = DateTime.Now,
+                    Note = "JavaScript Makale Yorumu"
+                });
+        }
+    }
+}
