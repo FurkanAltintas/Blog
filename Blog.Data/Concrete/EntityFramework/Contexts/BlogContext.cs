25c8d3b62d98bcb7850bd9f46f6da495fca9f921
Furkan Altıntaş <furkanaltintas785@gmail.com>
2022-03-31 16:09:05 +0300

    Context Added

---
+++ Blog.Data/Concrete/EntityFramework/Contexts/BlogContext.cs
@@ -0,0 +1,19 @@
+﻿using Blog.Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog.Data.Concrete.EntityFramework.Contexts
+{
+    public class BlogContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=DESKTOP-C8S1J58;Database=Blog;Trusted_Connection=True;Connect Timeout=30;MultipleActiveResultSets=True;");
+        }
+
+        public DbSet<Article> Articles { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
