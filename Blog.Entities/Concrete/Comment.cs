6e9fa91e537733ed2a32d1016136709a13064ea5
Furkan Altıntaş <furkanaltintas785@gmail.com>
2022-03-31 15:11:40 +0300

    Comment Entity Added

---
+++ Blog.Entities/Concrete/Comment.cs
@@ -0,0 +1,14 @@
+﻿using Blog.Shared.Entities.Abstract;
+
+namespace Blog.Entities.Concrete
+{
+    public class Comment : EntityBase, IEntity
+    {
+        public int ArticleId { get; set; }
+
+        public string Text { get; set; }
+
+
+        public Article Article { get; set; }
+    }
+}
