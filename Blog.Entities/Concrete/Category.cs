9876b82136414776b5c09e4afd5f872345973b43
Furkan Altıntaş <furkanaltintas785@gmail.com>
2022-03-31 14:53:23 +0300

    Category Entity Added

---
+++ Blog.Entities/Concrete/Category.cs
@@ -0,0 +1,12 @@
+﻿using Blog.Shared.Entities.Abstract;
+using System.Collections;
+
+namespace Blog.Entities.Concrete
+{
+    public class Category : EntityBase, IEntity
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public ICollection<Article> Articles { get; set; }
+    }
+}
