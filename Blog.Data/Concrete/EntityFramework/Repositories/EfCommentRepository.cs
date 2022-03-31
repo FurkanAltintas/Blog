53b6e325ae82773ec52e8453d7c87d7ec2725717
Furkan Altıntaş <furkanaltintas785@gmail.com>
2022-03-31 16:10:17 +0300

    Entity Framework CommentRepository Added

---
+++ Blog.Data/Concrete/EntityFramework/Repositories/EfCommentRepository.cs
@@ -0,0 +1,14 @@
+﻿using Blog.Data.Abstract;
+using Blog.Entities.Concrete;
+using Blog.Shared.Data.Concrete.EntityFramework;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog.Data.Concrete.EntityFramework.Repositories
+{
+    public class EfCommentRepository : EfEntityRepositoryBase<Comment>, ICommentRepository
+    {
+        public EfCommentRepository(DbContext context) : base(context)
+        {
+        }
+    }
+}
