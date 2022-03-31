d927c9f6a3e685d0b2bf6a695605204bca751c59
Furkan Altıntaş <furkanaltintas785@gmail.com>
2022-03-31 15:05:09 +0300

    Role Entity Added

---
+++ Blog.Entities/Concrete/Role.cs
@@ -0,0 +1,13 @@
+﻿using Blog.Shared.Entities.Abstract;
+using System.Collections.Generic;
+
+namespace Blog.Entities.Concrete
+{
+    public class Role : EntityBase, IEntity
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public ICollection<User> Users { get; set; }
+    }
+}
