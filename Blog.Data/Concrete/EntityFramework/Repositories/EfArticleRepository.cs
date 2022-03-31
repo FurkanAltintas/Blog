37b15d747ffe67fae917d9e424449f08e08cbcaf
Furkan Altıntaş <furkanaltintas785@gmail.com>
2022-03-31 16:09:59 +0300

    Entity Framework ArticleRepository Added

---
+++ Blog.Data/Concrete/EntityFramework/Repositories/EfArticleRepository.cs
@@ -0,0 +1,14 @@
+﻿using Blog.Data.Abstract;
+using Blog.Entities.Concrete;
+using Blog.Shared.Data.Concrete.EntityFramework;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog.Data.Concrete.EntityFramework.Repositories
+{
+    public class EfArticleRepository : EfEntityRepositoryBase<Article>, IArticleRepository
+    {
+        public EfArticleRepository(DbContext context) : base(context)
+        {
+        }
+    }
+}
