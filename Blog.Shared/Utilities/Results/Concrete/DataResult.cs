0907b49a5fd4341154eae4bcfbd313007f552b88
Furkan Altıntaş <furkanaltintas785@gmail.com>
2022-03-31 20:57:26 +0300

    DataResult Added

---
+++ Blog.Shared/Utilities/Results/Concrete/DataResult.cs
@@ -0,0 +1,38 @@
+﻿using Blog.Shared.Utilities.Results.Abstract;
+using Blog.Shared.Utilities.Results.ComplexTypes;
+using System;
+
+namespace Blog.Shared.Utilities.Results.Concrete
+{
+    public class DataResult<T> : IDataResult<T>
+    {
+        public DataResult(ResultStatus resultStatus, T data)
+        {
+            ResultStatus = resultStatus;
+            Data = data;
+        }
+
+        public DataResult(ResultStatus resultStatus, string message, T data)
+        {
+            ResultStatus = resultStatus;
+            Message = message;
+            Data = data;
+        }
+
+        public DataResult(ResultStatus resultStatus, string message, T data, Exception exception)
+        {
+            ResultStatus = resultStatus;
+            Message = message;
+            Data = data;
+            Exception = exception;
+        }
+
+        public T Data { get; }
+
+        public ResultStatus ResultStatus { get; }
+
+        public string Message { get; }
+
+        public Exception Exception { get; }
+    }
+}
