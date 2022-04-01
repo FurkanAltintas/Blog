49bc4e8bb6b0c4ea03f2677b892b1f881d4f43fd
Furkan Altıntaş <furkanaltintas785@gmail.com>
2022-04-01 15:06:57 +0300

    DtoGetBase Result Status Added

---
+++ Blog.Shared/Entities/Abstract/DtoGetBase.cs
@@ -0,0 +1,9 @@
+﻿using Blog.Shared.Utilities.Results.ComplexTypes;
+
+namespace Blog.Shared.Entities.Abstract
+{
+    public abstract class DtoGetBase
+    {
+        public virtual ResultStatus ResultStatus { get; set; }
+    }
+}
