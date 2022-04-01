98b7dbec21f1c8f4157ee0dd32ea31c4b34d1207
Furkan Altıntaş <furkanaltintas785@gmail.com>
2022-04-01 15:06:24 +0300

    Startup IServiceCollection Added

--- Blog.Mvc/Startup.cs
+++ Blog.Mvc/Startup.cs
@@ -1,12 +1,9 @@
+using Blog.Services.Extensions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Blog.Mvc
 {
@@ -16,6 +13,7 @@ namespace Blog.Mvc
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.LoadMyServices();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
