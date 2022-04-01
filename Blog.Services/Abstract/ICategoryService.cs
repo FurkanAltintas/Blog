f027e3f6a4dee99ce47cbbd87ea83cfaeece2d93
Furkan Altıntaş <furkanaltintas785@gmail.com>
2022-04-01 15:06:39 +0300

    Article & Category Service Added

---
+++ Blog.Services/Abstract/ICategoryService.cs
@@ -0,0 +1,18 @@
+﻿using Blog.Entities.Dtos;
+using Blog.Shared.Utilities.Results.Abstract;
+using System.Threading.Tasks;
+
+namespace Blog.Services.Abstract
+{
+    public interface ICategoryService
+    {
+        Task<IDataResult<CategoryDto>> Get(int categoryId);
+        Task<IDataResult<CategoryListDto>> GetAll();
+        Task<IDataResult<CategoryListDto>> GetAllByNonDeleted();
+        Task<IDataResult<CategoryListDto>> GetAllByNonDeletedAndActive();
+        Task<IResult> Add(CategoryAddDto categoryAddDto, string createdByName);
+        Task<IResult> Update(CategoryUpdateDto categoryUpdateDto, string modifiedByName);
+        Task<IResult> Delete(int categoryId, string modifiedByName);
+        Task<IResult> HardDelete(int categoryId);
+    }
+}
---
+++ Blog.Services/AutoMapper/Profiles/CategoryProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Blog.Entities.Concrete;
+using Blog.Entities.Dtos;
+using System;
+
+namespace Blog.Services.AutoMapper.Profiles
+{
+    public class CategoryProfile : Profile
+    {
+        public CategoryProfile()
+        {
+            CreateMap<CategoryAddDto, Category>().ForMember(dest => dest.CreatedDate, opt => opt.MapFrom(x => DateTime.Now));
+            CreateMap<CategoryUpdateDto, Category>().ForMember(dest => dest.ModifiedDate, opt => opt.MapFrom(x => DateTime.Now));
+        }
+    }
+}
