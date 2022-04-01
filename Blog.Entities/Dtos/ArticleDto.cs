d7cc61f83fe8f6d8139cfd8ff4d4da81459d16ab
Furkan Altıntaş <furkanaltintas785@gmail.com>
2022-04-01 15:06:09 +0300

    Article & Category Dto Added

---
+++ Blog.Entities/Dtos/ArticleDto.cs
@@ -0,0 +1,10 @@
+﻿using Blog.Entities.Concrete;
+using Blog.Shared.Entities.Abstract;
+
+namespace Blog.Entities.Dtos
+{
+    public class ArticleDto : DtoGetBase
+    {
+        public Article Article { get; set; }
+    }
+}
