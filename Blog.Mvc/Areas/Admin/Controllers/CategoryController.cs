fbfa3b5114540e7b6e86734109840358c2b82a96
Furkan Altıntaş <furkanaltintas785@gmail.com>
2022-04-02 22:19:34 +0300

    Category Update Added

--- Blog.Mvc/Areas/Admin/Controllers/CategoryController.cs
+++ Blog.Mvc/Areas/Admin/Controllers/CategoryController.cs
@@ -52,6 +52,21 @@ namespace Blog.Mvc.Areas.Admin.Controllers
             return Json(categoryAddAjaxErrorModel);
         }
 
+        [HttpGet]
+        public async Task<IActionResult> Update(int categoryId)
+        {
+            var result = await _categoryService.GetCategoryUpdateDto(categoryId);
+
+            if (result.ResultStatus == ResultStatus.Success)
+            {
+                return PartialView("_CategoryUpdatePartial", result.Data);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
         public async Task<JsonResult> GetAllCategories()
         {
             var result = await _categoryService.GetAllByNonDeleted();
