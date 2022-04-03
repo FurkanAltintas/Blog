56c492433c2a911d658bc9b2c007f1f47f2723f8
Furkan Altıntaş <furkanaltintas785@gmail.com>
2022-04-03 17:39:01 +0300

    UserController Added

---
+++ Blog.Mvc/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,114 @@
+﻿using AutoMapper;
+using Blog.Entities.Concrete;
+using Blog.Entities.Dtos;
+using Blog.Mvc.Areas.Admin.Models;
+using Blog.Shared.Utilities.Extensions;
+using Blog.Shared.Utilities.Results.ComplexTypes;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Blog.Mvc.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class UserController : Controller
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly IWebHostEnvironment _env;
+        private readonly IMapper _mapper;
+
+        public UserController(UserManager<User> userManager, IWebHostEnvironment env, IMapper mapper)
+        {
+            _userManager = userManager;
+            _env = env;
+            _mapper = mapper;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var users = await _userManager.Users.ToListAsync();
+            return View(new UserListDto
+            {
+                Users = users,
+                ResultStatus = ResultStatus.Success
+            });
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return PartialView("_UserAddPartial");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(UserAddDto userAddDto)
+        {
+            if (ModelState.IsValid)
+            {
+                userAddDto.Picture = await ImageUpload(userAddDto);
+                var user = _mapper.Map<User>(userAddDto);
+                var result = await _userManager.CreateAsync(user, userAddDto.Password);
+                if (result.Succeeded) // Bu işlem başarıyla gerçekleşti mi ?
+                {
+                    var userAddAjaxModel = JsonSerializer.Serialize(new UserAddAjaxViewModel
+                    {
+                        UserDto = new UserDto
+                        {
+                            ResultStatus = ResultStatus.Success,
+                            Message = $"{user.UserName} adlı kullanıcı adına sahip, kullanıcı başarıyla eklenmiştir.",
+                            User = user
+                        },
+                        UserAddPartial = await this.RenderViewToStringAsync("_UserAddPartial", userAddDto)
+                    });
+
+                    return Json(userAddAjaxModel);
+                }
+                else
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError(String.Empty, error.Description); // Description = Hata hakkında açıklama alanını içermektedir
+                    }
+
+                    var userAddAjaxErrorModel = JsonSerializer.Serialize(new UserAddAjaxViewModel
+                    {
+                        UserAddDto = userAddDto,
+                        UserAddPartial = await this.RenderViewToStringAsync("_UserAddPartial", userAddDto)
+                    });
+
+                    return Json(userAddAjaxErrorModel);
+                }
+            }
+            var userAddAjaxModelStateErrorModel = JsonSerializer.Serialize(new UserAddAjaxViewModel
+            {
+                UserAddDto = userAddDto,
+                UserAddPartial = await this.RenderViewToStringAsync("_UserAddPartial", userAddDto)
+            });
+
+            return Json(userAddAjaxModelStateErrorModel);
+        }
+
+        public async Task<string> ImageUpload(UserAddDto userAddDto)
+        {
+            string wwwroot = _env.WebRootPath; // string olarak wwwroot'un yolunu dinamik bir şekilde verir
+            // string fileName = Path.GetFileNameWithoutExtension(userAddDto.PictureFile.FileName); // Dosya adını aldık (Uzantısız bir şekilde aldık)
+            string fileExtension = Path.GetExtension(userAddDto.PictureFile.FileName); // Dosya uzantısını aldık
+            DateTime dateTime = DateTime.Now;
+            // FurkanAltintas_587_5_38_12_3_4_2022.jpg
+            string fileName = $"{userAddDto.UserName}_{dateTime.FullDateAndTimeStringWithUnderscore()}{fileExtension}";
+            var path = Path.Combine($"{wwwroot}/img", fileName);
+            await using (var stream = new FileStream(path, FileMode.Create))
+            {
+                await userAddDto.PictureFile.CopyToAsync(stream);
+            }
+
+            return fileName; // FurkanAltintas_575_3_23_11_2_4_2022.png
+        }
+    }
+}
