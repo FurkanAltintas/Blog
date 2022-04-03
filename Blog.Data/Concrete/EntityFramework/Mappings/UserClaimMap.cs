6fae304fd73ef4dbeda7b7f464f6e7b3b169dc89
Furkan Altıntaş <furkanaltintas785@gmail.com>
2022-04-03 17:34:28 +0300

    Identity Mapping Added

---
+++ Blog.Data/Concrete/EntityFramework/Mappings/UserClaimMap.cs
@@ -0,0 +1,18 @@
+﻿using Blog.Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Blog.Data.Concrete.EntityFramework.Mappings
+{
+    public class UserClaimMap : IEntityTypeConfiguration<UserClaim>
+    {
+        public void Configure(EntityTypeBuilder<UserClaim> builder)
+        {
+            // Primary key
+            builder.HasKey(uc => uc.Id);
+
+            // Maps to the AspNetUserClaims table
+            builder.ToTable("AspNetUserClaims");
+        }
+    }
+}
--- Blog.Data/Concrete/EntityFramework/Mappings/UserMap.cs
+++ Blog.Data/Concrete/EntityFramework/Mappings/UserMap.cs
@@ -1,8 +1,6 @@
 ﻿using Blog.Entities.Concrete;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
-using System;
-using System.Text;
 
 namespace Blog.Data.Concrete.EntityFramework.Mappings
 {
@@ -10,54 +8,43 @@ namespace Blog.Data.Concrete.EntityFramework.Mappings
     {
         public void Configure(EntityTypeBuilder<User> builder)
         {
+            // Primary key
             builder.HasKey(u => u.Id);
-            builder.Property(u => u.Id).ValueGeneratedOnAdd();
-            builder.Property(u => u.FirstName).IsRequired();
-            builder.Property(u => u.FirstName).HasMaxLength(30);
-            builder.Property(u => u.LastName).IsRequired();
-            builder.Property(u => u.LastName).HasMaxLength(30);
-            builder.Property(u => u.Email).IsRequired();
-            builder.Property(u => u.Email).HasMaxLength(50);
-            builder.HasIndex(u => u.Email).IsUnique();
-            builder.Property(u => u.UserName).IsRequired();
-            builder.Property(u => u.UserName).HasMaxLength(20);
-            builder.HasIndex(u => u.UserName).IsUnique();
-            builder.Property(u => u.PasswordHash).IsRequired();
-            builder.Property(u => u.PasswordHash).HasColumnType("VARBINARY(500)");
+
+            // Indexes for "normalized" username and email, to allow efficient lookups
+            builder.HasIndex(u => u.NormalizedUserName).HasDatabaseName("UserNameIndex").IsUnique();
+            builder.HasIndex(u => u.NormalizedEmail).HasDatabaseName("EmailIndex");
+
+            // Maps to the AspNetUsers table
+            builder.ToTable("AspNetUsers");
+
+            // A concurrency token for use with the optimistic concurrency checking
+            builder.Property(u => u.ConcurrencyStamp).IsConcurrencyToken();
+
+            // Limit the size of columns to use efficient database types
+            builder.Property(u => u.UserName).HasMaxLength(50);
+            builder.Property(u => u.NormalizedUserName).HasMaxLength(50);
+            builder.Property(u => u.Email).HasMaxLength(100);
+            builder.Property(u => u.NormalizedEmail).HasMaxLength(100);
+
+            // The relationships between User and other entity types
+            // Note that these relationships are configured with no navigation properties
+
+            // Each User can have many UserClaims
+            builder.HasMany<UserClaim>().WithOne().HasForeignKey(uc => uc.UserId).IsRequired();
+
+            // Each User can have many UserLogins
+            builder.HasMany<UserLogin>().WithOne().HasForeignKey(ul => ul.UserId).IsRequired();
+
+            // Each User can have many UserTokens
+            builder.HasMany<UserToken>().WithOne().HasForeignKey(ut => ut.UserId).IsRequired();
+
+            // Each User can have many entries in the UserRole join table
+            builder.HasMany<UserRole>().WithOne().HasForeignKey(ur => ur.UserId).IsRequired();
+
+
             builder.Property(u => u.Picture).IsRequired();
             builder.Property(u => u.Picture).HasMaxLength(250);
-            builder.Property(u => u.Description).HasMaxLength(500);
-            builder.Property(u => u.CreatedByName).IsRequired();
-            builder.Property(u => u.CreatedByName).HasMaxLength(50);
-            builder.Property(u => u.ModifiedByName).IsRequired();
-            builder.Property(u => u.ModifiedByName).HasMaxLength(50);
-            builder.Property(u => u.CreatedDate).IsRequired();
-            builder.Property(u => u.ModifiedDate).IsRequired();
-            builder.Property(u => u.IsActive).IsRequired();
-            builder.Property(u => u.IsDeleted).IsRequired();
-            builder.Property(u => u.Note).HasMaxLength(500);
-            builder.HasOne<Role>(u => u.Role).WithMany(r => r.Users).HasForeignKey(u => u.RoleId);
-            builder.ToTable("Users");
-
-            builder.HasData(new User
-            {
-                Id = 1,
-                RoleId = 1,
-                FirstName = "Furkan",
-                LastName = "Altıntaş",
-                UserName = "furkanaltintas",
-                Email = "furkanaltintas785@gmail.com",
-                Description = "İlk Admin Kullanıcı",
-                Picture = "https://m.media-amazon.com/images/M/MV5BMTQ3MzA3ODcwNF5BMl5BanBnXkFtZTgwNTI4NjIwMjE@._V1_.jpg",
-                PasswordHash = Encoding.ASCII.GetBytes("0192023a7bbd73250516f069df18b500"), // admin123
-                IsActive = true,
-                IsDeleted = false,
-                CreatedByName = "InitialCreate",
-                ModifiedByName = "InitialCreate",
-                CreatedDate = DateTime.Now,
-                ModifiedDate = DateTime.Now,
-                Note = "Admin Kullanıcısı"
-            });
         }
     }
 }
