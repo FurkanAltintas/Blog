efbc865fd3ec4b99d083133851877b51b053259d
Furkan Altıntaş <furkanaltintas785@gmail.com>
2022-04-03 17:37:00 +0300

    UserDtos Added

---
+++ Blog.Entities/Dtos/UserListDto.cs
@@ -0,0 +1,11 @@
+﻿using Blog.Entities.Concrete;
+using Blog.Shared.Entities.Abstract;
+using System.Collections.Generic;
+
+namespace Blog.Entities.Dtos
+{
+    public class UserListDto : DtoGetBase
+    {
+        public IList<User> Users { get; set; }
+    }
+}
