5e07bd1e3dc19e2c471ce8b0cf2cb64c228559ba
Furkan Altıntaş <furkanaltintas785@gmail.com>
2022-04-03 17:37:28 +0300

    Identity Added & UnitOfWork Deleted (Role, User)

--- Blog.Data/Concrete/UnitOfWork.cs
+++ Blog.Data/Concrete/UnitOfWork.cs
@@ -11,8 +11,6 @@ namespace Blog.Data.Concrete
         private EfArticleRepository _articleRepository;
         private EfCategoryRepository _categoryRepository;
         private EfCommentRepository _commentRepository;
-        private EfRoleRepository _roleRepository;
-        private EfUserRepository _userRepository;
 
         public UnitOfWork(BlogContext context)
         {
@@ -24,10 +22,6 @@ namespace Blog.Data.Concrete
         public ICategoryRepository Categories => _categoryRepository ?? new EfCategoryRepository(_context);
 
         public ICommentRepository Comments => _commentRepository ?? new EfCommentRepository(_context);
-
-        public IRoleRepository Roles => _roleRepository ?? new EfRoleRepository(_context);
-
-        public IUserRepository Users => _userRepository ?? new EfUserRepository(_context);
 
         public async ValueTask DisposeAsync()
         {
