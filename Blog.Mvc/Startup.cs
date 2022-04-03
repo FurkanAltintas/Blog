751db28b739ceef5c197d01ad05c5997bb58e4af
Furkan Altıntaş <furkanaltintas785@gmail.com>
2022-04-03 17:41:12 +0300

    AddSession, ConfigureApplicationCookie, UseAuthentication & UseAuthorization Added

--- Blog.Mvc/Startup.cs
+++ Blog.Mvc/Startup.cs
@@ -2,8 +2,10 @@ using Blog.Services.AutoMapper.Profiles;
 using Blog.Services.Extensions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System;
 using System.Text.Json.Serialization;
 
 namespace Blog.Mvc
@@ -19,8 +21,24 @@ namespace Blog.Mvc
                 options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
                 options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.Preserve;
             });
+            services.AddSession();
             services.AddAutoMapper(typeof(ArticleProfile), typeof(CategoryProfile));
             services.LoadMyServices();
+            services.ConfigureApplicationCookie(options =>
+            {
+                options.LoginPath = new PathString("/Admin/User/Login");
+                options.LogoutPath = new PathString("/Admin/User/Logout");
+                options.AccessDeniedPath = new PathString("/Admin/User/AccessDenied"); // Sistem içerisinde giriþ yapmýþ ama yetkisi olmayan bir yere ulaþýrsa bu sayfaya yönlendirilir
+                options.Cookie = new CookieBuilder
+                {
+                    Name = "Blog",
+                    HttpOnly = true, // Sadece http üzerinden göndermemizi saðlýyor
+                    SameSite = SameSiteMode.Strict, // Bu cookie bilgilerini sadece kendi sitemizden geldiðinde kabul edecek
+                    SecurePolicy = CookieSecurePolicy.SameAsRequest // Ýstek bizi http üzerinden gelirse http, https üzerinden gelirse https olur. (Bunun gerçek uygulamalarda kullanýmý Always olur. (Sadece https üzerinden olur.))
+                };
+                options.SlidingExpiration = true; // Kullanýcý tekrardan sisteme giriþ yaparsa oturum süresini tekrardan uzatýr
+                options.ExpireTimeSpan = TimeSpan.FromDays(7); // 7 gün boyunca kullanýcýnýn tekrardan giriþ yapmasýna gerek kalmýyacak.
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -31,14 +49,15 @@ namespace Blog.Mvc
                 app.UseDeveloperExceptionPage();
                 app.UseStatusCodePages();
             }
-
+            app.UseSession();
             app.UseStaticFiles();
             app.UseRouting();
-
+            app.UseAuthentication(); // Kimlik kontrolü
+            app.UseAuthorization(); // Yetki kontrolü
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapAreaControllerRoute(
-                    name:"Admin", areaName:"Admin", pattern:"Admin/{controller=Home}/{action=Index}/{id?}");
+                    name: "Admin", areaName: "Admin", pattern: "Admin/{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapDefaultControllerRoute();
             });
         }
