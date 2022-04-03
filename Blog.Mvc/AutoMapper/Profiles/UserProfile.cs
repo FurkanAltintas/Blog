26fd18f0ac5032145aa53b9cf94c482fdf06e0ff
Furkan Altıntaş <furkanaltintas785@gmail.com>
2022-04-03 17:39:53 +0300

    UI AutoMapper UserProfile Added

---
+++ Blog.Mvc/AutoMapper/Profiles/UserProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Blog.Entities.Concrete;
+using Blog.Entities.Dtos;
+
+namespace Blog.Mvc.AutoMapper.Profiles
+{
+    public class UserProfile : Profile
+    {
+        public UserProfile()
+        {
+            CreateMap<UserAddDto, User>(); // UserAddDto'da ki verileri User sınıfına map edeceğiz.
+        }
+    }
+}
