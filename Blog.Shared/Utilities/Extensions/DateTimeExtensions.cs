e8a75142df68877c527853a20cf2999f95d79278
Furkan Altıntaş <furkanaltintas785@gmail.com>
2022-04-03 17:35:36 +0300

    FileImage Date Added

---
+++ Blog.Shared/Utilities/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Blog.Shared.Utilities.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static string FullDateAndTimeStringWithUnderscore(this DateTime dateTime)
+        {
+            return $"{dateTime.Millisecond}_{dateTime.Second}_{dateTime.Minute}_{dateTime.Hour}_{dateTime.Day}_{dateTime.Month}_{dateTime.Year}";
+            /* FurkanAltintas_587_6_38_12_3_10_2022.jpg */
+        }
+    }
+}
