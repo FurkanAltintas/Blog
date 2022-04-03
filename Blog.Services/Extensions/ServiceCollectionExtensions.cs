3a648e76ab4ee0c48d72c8d26d42c97b6a817204
Furkan Altıntaş <furkanaltintas785@gmail.com>
2022-04-03 17:41:44 +0300

    AddIdentity Configuration Added

--- Blog.Services/Extensions/ServiceCollectionExtensions.cs
+++ Blog.Services/Extensions/ServiceCollectionExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using Blog.Data.Abstract;
 using Blog.Data.Concrete;
 using Blog.Data.Concrete.EntityFramework.Contexts;
+using Blog.Entities.Concrete;
 using Blog.Services.Abstract;
 using Blog.Services.Concrete;
 using Microsoft.Extensions.DependencyInjection;
@@ -12,6 +13,19 @@ namespace Blog.Services.Extensions
         public static IServiceCollection LoadMyServices(this IServiceCollection serviceCollection)
         {
             serviceCollection.AddDbContext<BlogContext>();
+            serviceCollection.AddIdentity<User, Role>(options =>
+            {
+                // User Password Options
+                options.Password.RequireDigit = false; // Rakam bulunsun mu ?
+                options.Password.RequiredLength = 6; // Zorunlu uzunluk karakteri (Kaç karakter olsun ?) // Minimum 10 veya 12 vermemiz daha iyi olur güvenlik için
+                options.Password.RequiredUniqueChars = 0; // Özel karakterlerden kaç tane olsun ? Unique karakterlerden kaç tane olması gerekiyor. (Özel karakterler !, ?)
+                options.Password.RequireNonAlphanumeric = false; // Özel karakterler kullanılsın mı ? Özel karakterlerin kullanılmasını sağlıyor. (@, !, ?, $)
+                options.Password.RequireLowercase = false; // Küçük harfler zorunlu olsun mu ?
+                options.Password.RequireUppercase = false; // Büyük harfler zorunlu olsun mu ?
+                // User UserName and Email Options
+                options.User.AllowedUserNameCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+$"; // Kullanıcı adı oluşturulurken verilen karakterler (Karakterleri yan yana yazmamız gerekmektedir.)
+                options.User.RequireUniqueEmail = true; // Bir kullanıcı kayıt olurken bu email adresinden sadece bir kayıt mı bulunsun ?
+            }).AddEntityFrameworkStores<BlogContext>();
             serviceCollection.AddScoped<IUnitOfWork, UnitOfWork>();
             serviceCollection.AddScoped<ICategoryService, CategoryManager>();
             serviceCollection.AddScoped<IArticleService, ArticleManager>();
