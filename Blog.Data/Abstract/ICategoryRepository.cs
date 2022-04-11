8b463fa1e320a9aca7d69863346ff789c7a8b095
Furkan Altıntaş <furkanaltintas785@gmail.com>
2022-04-11 16:00:28 +0300

    Category GetById Added

--- Blog.Data/Abstract/ICategoryRepository.cs
+++ Blog.Data/Abstract/ICategoryRepository.cs
@@ -1,9 +1,11 @@
 ﻿using Blog.Entities.Concrete;
 using Blog.Shared.Data.Abstract;
+using System.Threading.Tasks;
 
 namespace Blog.Data.Abstract
 {
     public interface ICategoryRepository : IEntityRepository<Category>
     {
+        Task<Category> GetByIdAsync(int categoryId);
     }
 }
