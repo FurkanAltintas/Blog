38b8794fb336ea2cd1433e4abbb33635eca300fe
Furkan Altıntaş <furkanaltintas785@gmail.com>
2022-04-12 16:09:27 +0300

    IArticleService Count Added

--- Blog.Services/Abstract/IArticleService.cs
+++ Blog.Services/Abstract/IArticleService.cs
@@ -15,5 +15,7 @@ namespace Blog.Services.Abstract
         Task<IResult> Update(ArticleUpdateDto articleUpdateDto, string modifiedByName);
         Task<IResult> Delete(int articleId, string modifiedByName);
         Task<IResult> HardDelete(int articleId);
+        Task<IDataResult<int>> Count();
+        Task<IDataResult<int>> CountByIsDeleted();
     }
 }
