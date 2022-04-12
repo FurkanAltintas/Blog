61ef7ad18ebd148ffa6ce37733a28073573ae7ce
Furkan Altıntaş <furkanaltintas785@gmail.com>
2022-04-12 17:39:47 +0300

    Dashboard Home Added

--- Blog.Mvc/Areas/Admin/Controllers/HomeController.cs
+++ Blog.Mvc/Areas/Admin/Controllers/HomeController.cs
@@ -1,5 +1,12 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using Blog.Entities.Concrete;
+using Blog.Mvc.Areas.Admin.Models;
+using Blog.Services.Abstract;
+using Blog.Shared.Utilities.Results.ComplexTypes;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
 
 namespace Blog.Mvc.Areas.Admin.Controllers
 {
@@ -7,9 +14,38 @@ namespace Blog.Mvc.Areas.Admin.Controllers
     [Authorize(Roles = "Admin,Editor")]
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly ICategoryService _categoryService;
+        private readonly IArticleService _articleService;
+        private readonly ICommentService _commentService;
+        private readonly UserManager<User> _userManager;
+
+        public HomeController(ICategoryService categoryService, IArticleService articleService, ICommentService commentService, UserManager<User> userManager)
         {
-            return View();
+            _categoryService = categoryService;
+            _articleService = articleService;
+            _commentService = commentService;
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var categoriesCountResult = await _categoryService.CountByNonDeleted();
+            var articlesCountResult = await _articleService.CountByNonDeleted();
+            var commentsCountResult = await _commentService.CountByNonDeleted();
+            var usersCount = await _userManager.Users.CountAsync();
+            var articlesResult = await _articleService.GetAll();
+            if (categoriesCountResult.ResultStatus == ResultStatus.Success && articlesCountResult.ResultStatus == ResultStatus.Success && commentsCountResult.ResultStatus == ResultStatus.Success && usersCount > -1 && articlesResult.ResultStatus == ResultStatus.Success)
+            {
+                return View(new DashboardViewModel
+                {
+                    CategoriesCount = categoriesCountResult.Data,
+                    ArticlesCount = articlesCountResult.Data,
+                    CommentsCount = commentsCountResult.Data,
+                    UsersCount = usersCount,
+                    Articles = articlesResult.Data
+                });
+            }
+            return NotFound();
         }
     }
 }
