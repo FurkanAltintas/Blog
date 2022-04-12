52bd504f3d72fa56e61dc79ea6bb93328ec0dabc
Furkan Altıntaş <furkanaltintas785@gmail.com>
2022-04-12 16:08:32 +0300

    Shared Extension TitleNaming Added

---
+++ Blog.Shared/Utilities/Extensions/TitleNamingExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Blog.Shared.Utilities.Extensions
+{
+    public static class TitleNamingExtensions
+    {
+        public static string TitleNameByControllerName(this string controller)
+        {
+            switch (controller)
+            {
+                case "Home":
+                    return "Yönetim Paneli";
+                case "Article":
+                    return "Makaleler";
+                case "Category":
+                    return "Kategoriler";
+                case "User":
+                    return "Kullanıcılar";
+                default:
+                    return "";
+            }
+        }
+    }
+}
