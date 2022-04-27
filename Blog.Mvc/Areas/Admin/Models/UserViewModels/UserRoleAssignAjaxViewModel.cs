24670237becac9f5bc988dd3131ea99504253560
Furkan Altıntaş <furkanaltintas785@gmail.com>
2022-04-27 18:13:12 +0300

    Mvc Areas Admin Models UserViewModels Added

---
+++ Blog.Mvc/Areas/Admin/Models/UserViewModels/UserRoleAssignAjaxViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Blog.Entities.Dtos;
+
+namespace Blog.Mvc.Areas.Admin.Models
+{
+    public class UserRoleAssignAjaxViewModel
+    {
+        public UserRoleAssingDto UserRoleAssingDto { get; set; }
+        public string RoleAssignPartial { get; set; }
+        public UserDto UserDto { get; set; }
+    }
+}
