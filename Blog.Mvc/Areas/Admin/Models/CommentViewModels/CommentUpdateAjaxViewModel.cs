4d616f4a676668f250f4815450edf20b52f6fe4f
Furkan Altıntaş <furkanaltintas785@gmail.com>
2022-04-27 18:13:39 +0300

    Mvc Areas Admin Models CommentViewModels Added

---
+++ Blog.Mvc/Areas/Admin/Models/CommentViewModels/CommentUpdateAjaxViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Blog.Entities.Dtos;
+
+namespace Blog.Mvc.Areas.Admin.Models
+{
+    public class CommentUpdateAjaxViewModel
+    {
+        public CommentUpdateDto CommentUpdateDto { get; set; }
+        public string CommentUpdatePartial { get; set; }
+        public CommentDto CommentDto { get; set; }
+    }
+}
