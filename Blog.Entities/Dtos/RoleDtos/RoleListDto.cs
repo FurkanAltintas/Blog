b36a935af30ae6040b867dc2c8356909896c5c87
Furkan Altıntaş <furkanaltintas785@gmail.com>
2022-04-27 18:10:57 +0300

    Entities Dtos RoleDtos Added

---
+++ Blog.Entities/Dtos/RoleDtos/RoleListDto.cs
@@ -0,0 +1,10 @@
+﻿using Blog.Entities.Concrete;
+using System.Collections.Generic;
+
+namespace Blog.Entities.Dtos
+{
+    public class RoleListDto
+    {
+        public IList<Role> Roles { get; set; }
+    }
+}
