12f059bfd0453a3d76568c148d47b9757862efb5
Furkan Altıntaş <furkanaltintas785@gmail.com>
2022-04-27 18:11:05 +0300

    Entities Dtos UserDtos Added

---
+++ Blog.Entities/Dtos/UserDtos/UserRoleAssingDto.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace Blog.Entities.Dtos
+{
+    public class UserRoleAssingDto
+    {
+        public UserRoleAssingDto()
+        {
+            RoleAssignDtos = new List<RoleAssignDto>();
+        }
+
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public IList<RoleAssignDto> RoleAssignDtos { get; set; }
+    }
+}
