961d52a1e81d9a71d8d9f667c52691a36ecb8c1a
Furkan Altıntaş <furkanaltintas785@gmail.com>
2022-04-27 18:10:39 +0300

    Entities Dtos CommentDtos Added

---
+++ Blog.Entities/Dtos/CommentDtos/CommentDto.cs
@@ -0,0 +1,9 @@
+﻿using Blog.Entities.Concrete;
+
+namespace Blog.Entities.Dtos
+{
+    public class CommentDto
+    {
+        public Comment Comment { get; set; }
+    }
+}
---
+++ Blog.Entities/Dtos/CommentDtos/CommentListDto.cs
@@ -0,0 +1,10 @@
+﻿using Blog.Entities.Concrete;
+using System.Collections.Generic;
+
+namespace Blog.Entities.Dtos
+{
+    public class CommentListDto
+    {
+        public IList<Comment> Comments { get; set; }
+    }
+}
