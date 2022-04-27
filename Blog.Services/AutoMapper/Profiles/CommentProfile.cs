ac2359aeb97e7fc86c3f085bcbec43051cadaf4b
Furkan Altıntaş <furkanaltintas785@gmail.com>
2022-04-27 18:15:57 +0300

    Services AutoMapper Profiles Added

---
+++ Blog.Services/AutoMapper/Profiles/CommentProfile.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using Blog.Entities.Concrete;
+using Blog.Entities.Dtos;
+using System;
+
+namespace Blog.Services.AutoMapper.Profiles
+{
+    public class CommentProfile : Profile
+    {
+        public CommentProfile()
+        {
+            CreateMap<CommentAddDto, Comment>()
+                .ForMember(dest => dest.CreatedDate, opt => opt.MapFrom(x => DateTime.Now))
+                .ForMember(dest => dest.ModifiedDate, opt => opt.MapFrom(x => DateTime.Now))
+                .ForMember(dest => dest.ModifiedByName, opt => opt.MapFrom(x => x.CreatedByName))
+                .ForMember(dest => dest.IsActive, opt => opt.MapFrom(x => false));
+            CreateMap<CommentUpdateDto, Comment>()
+                .ForMember(dest => dest.ModifiedDate, opt => opt.MapFrom(x => DateTime.Now));
+            CreateMap<Comment, CommentUpdateDto>();
+
+        }
+    }
+}
