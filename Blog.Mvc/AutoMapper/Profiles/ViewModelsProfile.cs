b882c36e4b7115f5b8978cc74375425e21c73ceb
Furkan Altıntaş <furkanaltintas785@gmail.com>
2022-04-27 18:18:31 +0300

    Mvc AutoMapper Profiles ViewModelsProfile Added

---
+++ Blog.Mvc/AutoMapper/Profiles/ViewModelsProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Blog.Entities.Dtos;
+using Blog.Mvc.Areas.Admin.Models;
+
+namespace Blog.Mvc.AutoMapper.Profiles
+{
+    public class ViewModelsProfile : Profile
+    {
+        public ViewModelsProfile()
+        {
+            CreateMap<ArticleAddViewModel, ArticleAddDto>();
+            CreateMap<ArticleUpdateDto, ArticleUpdateViewModel>().ReverseMap();
+        }
+    }
+}
