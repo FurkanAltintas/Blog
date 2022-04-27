74bd33b8d1413fb2c9547553cf99bbeaf1054d6f
Furkan Altıntaş <furkanaltintas785@gmail.com>
2022-04-27 18:10:28 +0300

    Entities Dtos CategoryDtos Added

---
+++ Blog.Entities/Dtos/CategoryDtos/CategoryAddDto.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Blog.Entities.Dtos
+{
+    public class CategoryAddDto
+    {
+        [DisplayName("Kategori Adı")]
+        [Required(ErrorMessage = "{0} boş geçilmemelidir.")]
+        [MaxLength(70, ErrorMessage = "{0} {1} karakterden büyük olmamalıdır.")]
+        [MinLength(3, ErrorMessage = "{0} {1} karakterden küçük olmamalıdır.")]
+        public string Name { get; set; }
+
+        [DisplayName("Kategori Açıklaması")]
+        [MaxLength(500, ErrorMessage = "{0} {1} karakterden büyük olmamalıdır.")]
+        [MinLength(3, ErrorMessage = "{0} {1} karakterden küçük olmamalıdır.")]
+        public string Description { get; set; }
+
+        [DisplayName("Kategori Özel Not Alanı")]
+        [Required(ErrorMessage = "{0} boş geçilmemelidir.")]
+        [MaxLength(500, ErrorMessage = "{0} {1} karakterden büyük olmamalıdır.")]
+        [MinLength(3, ErrorMessage = "{0} {1} karakterden az olmamalıdır.")]
+        public string Note { get; set; }
+
+        [DisplayName("Aktif Mi?")]
+        [Required(ErrorMessage = "{0} boş geçilmemelidir.")]
+        public bool IsActive { get; set; }
+    }
+}
