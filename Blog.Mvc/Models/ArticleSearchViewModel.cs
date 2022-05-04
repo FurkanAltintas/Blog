f342d3fa9cc4c76c2f5b5d71486a180598b31df4
Furkan Altıntaş <furkanaltintas785@gmail.com>
2022-05-04 18:00:45 +0300

    ArticleSearchViewModel Added

---
+++ Blog.Mvc/Models/ArticleSearchViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Blog.Entities.Dtos;
+
+namespace Blog.Mvc.Models
+{
+    public class ArticleSearchViewModel
+    {
+        public ArticleListDto ArticleListDto { get; set; }
+        public string Keyword { get; set; }
+    }
+}
