1eb3f41aab176b407528d17399cba5b2315423ba
Furkan Altıntaş <furkanaltintas785@gmail.com>
2022-05-04 18:01:43 +0300

    RightSideBarViewComponent Added

---
+++ Blog.Mvc/ViewComponents/RightSideBarViewComponent.cs
@@ -0,0 +1,30 @@
+﻿using Blog.Mvc.Models;
+using Blog.Services.Abstract;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Blog.Mvc.ViewComponents
+{
+    public class RightSideBarViewComponent : ViewComponent
+    {
+        private readonly IArticleService _articleService;
+        private readonly ICategoryService _categoryService;
+
+        public RightSideBarViewComponent(IArticleService articleService, ICategoryService categoryService)
+        {
+            _articleService = articleService;
+            _categoryService = categoryService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var categoriesResult = await _categoryService.GetAllByNonDeletedAndActiveAsync();
+            var articlesResult = await _articleService.GetAllByViewCountAsync(isAscending: false, takeSize: 5);
+            return View(new RightSideBarViewModel
+            {
+                Articles = articlesResult.Data.Articles,
+                Categories = categoriesResult.Data.Categories
+            });
+        }
+    }
+}
