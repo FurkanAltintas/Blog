d11ecfefe40052d69b4e9e624d75cc6f364a74c8
Furkan Altıntaş <furkanaltintas785@gmail.com>
2022-05-04 17:59:36 +0300

    ArticleListDto Pager Added

--- Blog.Entities/Dtos/ArticleDtos/ArticleListDto.cs
+++ Blog.Entities/Dtos/ArticleDtos/ArticleListDto.cs
@@ -7,5 +7,6 @@ namespace Blog.Entities.Dtos
     public class ArticleListDto : DtoGetBase
     {
         public IList<Article> Articles { get; set; }
+        public int? CategoryId { get; set; }
     }
 }
