c7e76b7183cd356c35418fa1de488f0f9fee5e8d
Furkan Altıntaş <furkanaltintas785@gmail.com>
2022-05-04 18:00:12 +0300

    ArticleController Added

---
+++ Blog.Mvc/Controllers/ArticleController.cs
@@ -0,0 +1,41 @@
+﻿using Blog.Mvc.Models;
+using Blog.Services.Abstract;
+using Blog.Shared.Utilities.Results.ComplexTypes;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Blog.Mvc.Controllers
+{
+    public class ArticleController : Controller
+    {
+        private readonly IArticleService _articleService;
+
+        public ArticleController(IArticleService articleService)
+        {
+            _articleService = articleService;
+        }
+
+        public async Task<IActionResult> Search(string keyword, int currentPage = 1, int pageSize = 5, bool isAscending = false)
+        {
+            var searchResult = await _articleService.SearchAsync(keyword, currentPage, pageSize, isAscending);
+            if (searchResult.ResultStatus == ResultStatus.Success)
+                return View(new ArticleSearchViewModel
+                {
+                    ArticleListDto = searchResult.Data,
+                    Keyword = keyword
+                });
+            return NotFound();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Detail(int articleId)
+        {
+            var articleResult = await _articleService.GetAsync(articleId);
+            if (articleResult.ResultStatus == ResultStatus.Success)
+            {
+                return View(articleResult.Data);
+            }
+            return NotFound();
+        }
+    }
+}
