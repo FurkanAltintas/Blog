6c0f28f7d0e2fec2787884953c1bec440b328c55
Furkan Altıntaş <furkanaltintas785@gmail.com>
2022-05-04 18:00:23 +0300

    CommentController Added

---
+++ Blog.Mvc/Controllers/CommentController.cs
@@ -0,0 +1,50 @@
+﻿using Blog.Entities.Dtos;
+using Blog.Mvc.Models;
+using Blog.Services.Abstract;
+using Blog.Shared.Utilities.Extensions;
+using Blog.Shared.Utilities.Results.ComplexTypes;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Blog.Mvc.Controllers
+{
+    public class CommentController : Controller
+    {
+        private readonly ICommentService _commentService;
+
+        public CommentController(ICommentService commentService)
+        {
+            _commentService = commentService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(CommentAddDto commentAddDto)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _commentService.AddAsync(commentAddDto);
+                if (result.ResultStatus == ResultStatus.Success)
+                {
+                    var commentAddAjaxViewModel = JsonSerializer.Serialize(new CommentAddAjaxViewModel
+                    {
+                        CommentDto = result.Data,
+                        CommentAddPartial = await this.RenderViewToStringAsync("_CommentAddPartial", commentAddDto)
+                    }, new JsonSerializerOptions
+                    {
+                        ReferenceHandler = ReferenceHandler.Preserve
+                    });
+                    return Json(commentAddAjaxViewModel);
+                }
+                ModelState.AddModelError("", result.Message);
+            }
+            var commentAddAjaxErrorModel = JsonSerializer.Serialize(new CommentAddAjaxViewModel
+            {
+                CommentAddDto = commentAddDto,
+                CommentAddPartial = await this.RenderViewToStringAsync("_CommentAddPartial", commentAddDto)
+            });
+            return Json(commentAddAjaxErrorModel);
+        }
+    }
+}
