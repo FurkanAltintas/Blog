dc62ab6f8d1a763eb2d83b4acad7a03468ed9e58
Furkan Altıntaş <furkanaltintas785@gmail.com>
2022-05-04 18:00:33 +0300

    HomeController Added

--- Blog.Mvc/Controllers/HomeController.cs
+++ Blog.Mvc/Controllers/HomeController.cs
@@ -1,12 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Blog.Services.Abstract;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace Blog.Mvc.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly IArticleService _articleService;
+
+        public HomeController(IArticleService articleService)
         {
-            return View();
+            _articleService = articleService;
+        }
+
+        public async Task<IActionResult> Index(int? categoryId, int currentPage = 1, int pageSize = 5, bool isAscending = false)
+        {
+            var articleResult = await (categoryId == null
+                ? _articleService.GetAllByPagingAsync(null, currentPage, pageSize, isAscending)
+                : _articleService.GetAllByPagingAsync(categoryId.Value, currentPage, pageSize, isAscending));
+            return View(articleResult.Data);
         }
     }
 }
