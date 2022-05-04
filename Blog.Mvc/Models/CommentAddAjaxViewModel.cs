c997dd42bb87f679ed5710834f673df0233884ea
Furkan Altıntaş <furkanaltintas785@gmail.com>
2022-05-04 18:00:56 +0300

    CommentAddAjaxViewModel Added

---
+++ Blog.Mvc/Models/CommentAddAjaxViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Blog.Entities.Dtos;
+
+namespace Blog.Mvc.Models
+{
+    public class CommentAddAjaxViewModel
+    {
+        public CommentAddDto CommentAddDto { get; set; }
+        public string CommentAddPartial { get; set; }
+        public CommentDto CommentDto { get; set; }
+    }
+}
