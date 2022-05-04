e5223c858949fbefdeb2c8e4e4aeb02a27473f9d
Furkan Altıntaş <furkanaltintas785@gmail.com>
2022-05-04 18:04:15 +0300

    DtoGetBase Paging Added

--- Blog.Shared/Entities/Abstract/DtoGetBase.cs
+++ Blog.Shared/Entities/Abstract/DtoGetBase.cs
@@ -1,4 +1,5 @@
 ﻿using Blog.Shared.Utilities.Results.ComplexTypes;
+using System;
 
 namespace Blog.Shared.Entities.Abstract
 {
@@ -6,5 +7,12 @@ namespace Blog.Shared.Entities.Abstract
     {
         public virtual ResultStatus ResultStatus { get; set; }
         public virtual string Message { get; set; }
+        public virtual int CurrentPage { get; set; } = 1; // Şu an ki sayfası
+        public virtual int PageSize { get; set; } = 5; // Bir sayfadaki içerik sayısı
+        public virtual int TotalCount { get; set; } // Toplam içerik sayısı
+        public virtual int TotalPages => (int)Math.Ceiling(decimal.Divide(TotalCount, PageSize)); // Toplam sayfa sayısı
+        public virtual bool ShowPrevious => CurrentPage > 1; // Sayfa 1 den büyük ise bir önceki sayfaya gidebiliriz.
+        public virtual bool ShowNext => CurrentPage < TotalPages; // Toplam sayfamızdan küçük ise bir sonraki sayfaya gidebiliriz.
+        public virtual bool IsAscending { get; set; } = false;
     }
 }
