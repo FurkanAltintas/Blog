e18ea15d3fa5b243cb4f5726e3a74a95f09ed875
Furkan Altıntaş <furkanaltintas785@gmail.com>
2022-05-04 18:01:08 +0300

    RightSideBarViewModel Added

---
+++ Blog.Mvc/Models/RightSideBarViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Blog.Entities.Concrete;
+using System.Collections.Generic;
+
+namespace Blog.Mvc.Models
+{
+    public class RightSideBarViewModel
+    {
+        public IList<Category> Categories { get; set; }
+        public IList<Article> Articles { get; set; }
+    }
+}
